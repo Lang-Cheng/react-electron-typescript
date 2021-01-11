6e3ff8353098db005f8db99ed9876567cf2d76ab
Lang-Cheng <lang_cl@outlook.com>
2021-01-11 14:48:05 +0800

    Upgrade dependency

--- electron/main.ts
+++ electron/main.ts
@@ -1,20 +1,7 @@
-/*
- * @Author: Lang Cheng
- * @Date: 2021-01-10 19:14:16
- * @LastEditTime: 2021-01-11 12:06:50
- * @LastEditors: Lang Cheng
- * @Description: electron
- * @FilePath: \2048\electron\main.ts
- */
-
 import * as path from 'path';
 
 /* eslint-disable import/no-extraneous-dependencies */
 import { app, BrowserWindow } from 'electron';
-import installExtension, {
-    REACT_DEVELOPER_TOOLS,
-    REDUX_DEVTOOLS
-} from 'electron-devtools-installer';
 /* eslint-enable import/no-extraneous-dependencies */
 
 let mainWindow: Electron.BrowserWindow | null;
@@ -25,33 +12,38 @@ function createWindow() {
         height: 700,
         backgroundColor: '#191622',
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation: false
         }
     });
 
     if (process.env.NODE_ENV === 'development') {
         mainWindow.loadURL('http://localhost:4000');
+        // Open the DevTools.
+        mainWindow.webContents.openDevTools();
     } else {
         mainWindow.loadFile(path.join(__dirname, 'renderer/index.html'));
     }
-
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
 }
 
-/* eslint-disable no-console */
-app.on('ready', createWindow)
-    .whenReady()
-    .then(() => {
-        if (process.env.NODE_ENV === 'development') {
-            installExtension(REACT_DEVELOPER_TOOLS)
-                .then((name) => console.log(`Added Extension:  ${name}`))
-                .catch((err) => console.log('An error occurred: ', err));
-            installExtension(REDUX_DEVTOOLS)
-                .then((name) => console.log(`Added Extension:  ${name}`))
-                .catch((err) => console.log('An error occurred: ', err));
-        }
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+    createWindow();
+
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
-app.allowRendererProcessReuse = true;
-/* eslint-enable no-console */
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
